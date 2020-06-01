be3668447f84c7f6490af9b32b2e55f5ee8e6fa8
acteryano <acteryano@users.noreply.github.com>
2020-06-01 11:53:13 +0100

    Update cal.js

--- cal.js
+++ cal.js
@@ -4,7 +4,6 @@ var operators = ['+', '-', 'x', '÷'];
 var decimalAdded = false;
 for(var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(e) {
-		// Get the input and button values
 		var input = document.querySelector('.screen');
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
